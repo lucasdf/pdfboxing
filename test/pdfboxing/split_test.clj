71696b2ae9a91196041538deae689533d32d6e7d
Александар Симић <asimic@gmail.com>
2019-07-08 20:48:44 +0100

    Fix for split-pdf
    Previously with-open was used to open the PDF document that would be
    split. Which is fine if you're just thinking about closing the file
    after you've split it. But it doesn't seem very useful, because once
    you've split the file, you probably want to do something with it.
    
    I'm reverting this change introduced via
    2ed58248360788cbdd4a4b5dbc95eadaddd9c767 just to get some opinions in
    this issue: https://github.com/dotemacs/pdfboxing/issues/46

--- test/pdfboxing/split_test.clj
+++ test/pdfboxing/split_test.clj
@@ -1,6 +1,8 @@
 (ns pdfboxing.split-test
-  (:require [clojure.test :refer [deftest is]]
-            [pdfboxing.split :as split]))
+  (:require [clojure.test :refer [deftest is testing]]
+            [pdfboxing.split :as split]
+            [pdfboxing.common :refer [is-pdf?]]
+            [pdfboxing.info :refer [page-number]]))
 
 (deftest pdf-input-arg-check
   "Test to ensure that the invalid input throws meaningful exception"
@@ -31,3 +33,13 @@
                (finally
                  (when (instance? org.apache.pdfbox.pdmodel.PDDocument doc)
                    (.close doc))))))))
+
+
+(deftest pdf-file-split-and-document-generation
+  (testing "That the split PDF file generates a new PDF file"
+    (let [_ (split/merge-pddocuments :docs (split/split-pdf :input "test/pdfs/multi-page.pdf"
+                                                            :start 1
+                                                            :end 2)
+                                     :output "split.pdf")]
+      (is (true? (is-pdf? output)))
+      (is (= 2 (page-number output))))))
